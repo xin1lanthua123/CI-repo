d74ba64021083fc20afab8c322b6d74e461fb0a7
quanldl <ldlq2005@gmail.com>
2026-05-09 00:48:26 +0700

    update

--- infra/bootstrap/bootstrap_role.tf
+++ infra/bootstrap/bootstrap_role.tf
@@ -1,15 +1,3 @@
-data "aws_caller_identity" "current" {}
-data "tls_certificate" "github" {
-  url = "https://token.actions.githubusercontent.com"
-}
-resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  thumbprint_list = [for cert in data.tls_certificate.github.certificates: cert.sha1_fingerprint]
-}
-
 resource "aws_iam_role" "github_actions_bootstrap" {
   name = "${var.project_name}-github-bootstrap-role"
 

386037ac29eef56d6d904424b1c7a0a8c68feb56
quanldl <ldlq2005@gmail.com>
2026-06-01 17:47:55 +0700

    update dockerfile lint job in git file

--- services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfo.cs
+++ services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c1b7ee6d28a2678e9e03150b7953b5fa265b96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59645f553b3fe48d3623e294c2a48c8975f3c893")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
+++ services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0907b030c3b602d0248b4aa35ca05137c0f012b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59645f553b3fe48d3623e294c2a48c8975f3c893")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

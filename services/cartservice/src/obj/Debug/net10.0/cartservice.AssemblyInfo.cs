a638ebc7c977fb7ad292c0d9bd821d410b36e541
quanldl <ldlq2005@gmail.com>
2026-06-05 00:30:11 +0700

    update src code

--- services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfo.cs
+++ services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6fae8437698f97baa8c8e542a6c6948f8f75da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d332e82805a77752b94f39f2010f02b11e11f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

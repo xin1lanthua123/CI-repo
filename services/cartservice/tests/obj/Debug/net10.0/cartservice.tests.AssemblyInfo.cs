0f329cf65f7b12114bfa4476dc3a69e4689dcdea
quanldl <ldlq2005@gmail.com>
2026-06-08 21:43:52 +0700

    update git flle -> workflow run on all microservices

--- services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
+++ services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3811087e54373bd65565278358b307a179de9d25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e688b21231c2bfb0035f67b39565d0b8e54e7f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

268c7aa95b21195e165c8817ad1de1a80c034f07
quanldl <ldlq2005@gmail.com>
2026-07-25 13:32:57 +0700

    modified:   services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfo.cs         modified:   services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfoInputs.cache         modified:   services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs         modified:   services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfoInputs.cache

--- services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
+++ services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf0828f9d22aab17a03f6cb55b446184e8588655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36ca86d4d85ea2fc90b5141a9e11f4169cb77ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

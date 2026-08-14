fc7ac53dab344fee35655ef4bf16dbeb88b45ba7
quanldl <ldlq2005@gmail.com>
2026-08-14 22:28:54 +0700

    modified:   .github/workflows/demo.yaml         modified:   services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfo.cs         modified:   services/cartservice/src/obj/Debug/net10.0/cartservice.AssemblyInfoInputs.cache         modified:   services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs         modified:   services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfoInputs.cache

--- services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
+++ services/cartservice/tests/obj/Debug/net10.0/cartservice.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c6032ebf9f5031f4767fffb012edcae4e58a44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e23c09cf85f2e1d469e6d7ceff096ece5b5e42")]
 [assembly: System.Reflection.AssemblyProductAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cartservice.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
